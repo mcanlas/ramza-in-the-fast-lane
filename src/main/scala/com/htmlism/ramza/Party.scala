e225077ce23ba81afdd08597c01267fa013b9307
Mark Canlas <github@htmlism.com>
2013-11-06 11:33:33 -0500

    added gain experience workflow

--- src/main/scala/com/htmlism/ramza/Party.scala
+++ src/main/scala/com/htmlism/ramza/Party.scala
@@ -1,3 +1,20 @@
 package com.htmlism.ramza
 
-case class Party(characters: ZodiacWarrior*)
+import com.htmlism.ramza.Jobs.JobClass
+import scala.annotation.tailrec
+
+case class Party(characters: ZodiacWarrior*) {
+  private def gainExperienceByJob(parties: Parties, character: ZodiacWarrior, jobs: Traversable[JobClass]) = ???
+
+  @tailrec
+  private def gainExperienceByCharacter(parties: Parties, characters: Traversable[ZodiacWarrior]): Parties = characters match {
+    case head :: tail => {
+      val partiesFromOneCharacter = gainExperienceByJob(parties, head, ???)
+
+      gainExperienceByCharacter(partiesFromOneCharacter, tail)
+    }
+    case Nil => parties
+  }
+
+  def gainExperience: Parties = gainExperienceByCharacter(this :: Nil, characters)
+}
--- src/main/scala/com/htmlism/ramza/package.scala
+++ src/main/scala/com/htmlism/ramza/package.scala
@@ -2,6 +2,7 @@ package com.htmlism
 
 package object ramza {
   type JobId = Int
+  type Parties = List[Party]
 
   val jobPointMinimums = List(200, 400, 700, 1100, 1600, 2200, 3000)
 
