c04a4459c2758e53b5e7d23409b57e07c23c0e0a
Mark Canlas <github@htmlism.com>
2013-11-06 00:49:47 -0500

    added tail recursion assertion

--- src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
+++ src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
@@ -3,7 +3,10 @@ package com.htmlism.ramza
 case class ZodiacWarrior(jobPoints: Map[JobId, Int] = Map(1 -> 0, 2 -> 0), experiencePoints: Int = 100) {
   def level = experiencePoints / 100
 
-  def jobLevel(job: JobId, minimumsToCheck: List[Int] = jobPointMinimums, level: Int = 1): Int = minimumsToCheck match {
+  import scala.annotation.tailrec
+
+  @tailrec
+  final def jobLevel(job: JobId, minimumsToCheck: List[Int] = jobPointMinimums, level: Int = 1): Int = minimumsToCheck match {
     case jobPointMinimum :: remainingMinimums =>
       if (jobPoints(job) >= jobPointMinimum)
         jobLevel(job, remainingMinimums, level + 1)
