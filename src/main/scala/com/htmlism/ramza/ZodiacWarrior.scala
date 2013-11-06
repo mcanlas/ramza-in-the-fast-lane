bcff1f75ad0f3b5888086a5ad759f3f8ed25e11a
Mark Canlas <github@htmlism.com>
2013-11-06 00:25:37 -0500

    added introspection stubs

--- src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
+++ src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
@@ -1,3 +1,9 @@
 package com.htmlism.ramza
 
-case class ZodiacWarrior(jobPoints: Map[JobId, Int], experiencePoints: Int = 100)
+case class ZodiacWarrior(jobPoints: Map[JobId, Int] = Map(1 -> 0, 2 -> 0), experiencePoints: Int = 100) {
+  def level = experiencePoints / 100
+
+  def jobLevel(job: JobId, minimumsToCheck: List[Int] = jobPointMinimums, level: Int = 1) = ???
+
+  def availableJobs = ???
+}
