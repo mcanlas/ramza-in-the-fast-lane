81ac748b9a1b01ebaa8ffd4788b40802cd5c7a9d
Mark Canlas <github@htmlism.com>
2013-11-06 12:19:22 -0500

    added prerequisite parsing

--- jobs.pl
+++ jobs.pl
@@ -5,16 +5,39 @@ use warnings;
 
 use 5.10.0;
 
+my %jobs;
+
 chomp(my @lines = <STDIN>);
 
 my @jobs = map {
   my($id, $class, @prerequisites) = split /, /;
 
+  @prerequisites = map {
+    s/\*/8/g;
+
+    my($job, $level) = $_ =~ /(.+) (\d)/;
+
+    [$job, int $level]
+  } @prerequisites;
+
+  $jobs{ $class } = { map { @$_ } @prerequisites };
+
+  for (@prerequisites) {
+    my($job, $level) = @$_;
+    my %more_prerequisites = (%{ $jobs{ $job } }, $job, $level);
+
+    while (my($k, $v) = each %more_prerequisites) {
+      ${ $jobs{ $class } }{ $k } = $v unless exists ${ $jobs{ $class } }{ $k } and ${ $jobs{ $class } }{ $k } > $v;
+    }
+  }
+
   my $value_name = uc $class =~ s/ /_/r;
   my $object_name = $class =~ s/ //r;
 
-  [$id, $class, $value_name, $object_name]
+  [$id, $class, $value_name, $object_name, \@prerequisites]
 } @lines;
+
+use Data::Dumper; print Dumper \%jobs;
 
 my $longest_name_length = do {
   my $champion = (sort { length $b->[2] <=> length $a->[2] } @jobs)[0][2];
@@ -34,4 +57,12 @@ say join ', ', map { $_->[3] } @jobs;
 
 for (sort { $a->[0] <=> $b->[0] } @jobs) {
   say sprintf "%-${longest_name_length}s -> 0,", $_->[3];
+}
+
+for (sort { $a->[0] <=> $b->[0] } @jobs) {
+  my $tuples = join ', ', map { 1 } @{ $_->[4] };
+
+  my $map = @{ $_->[4] } ? "Map($tuples)" : "Map[JobClass, Int]()";
+
+  say sprintf "%-${longest_name_length}s -> $map,", $_->[3];
 }
