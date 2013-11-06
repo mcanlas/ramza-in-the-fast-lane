815a8fdc4a130c49559ee5cf8b86f89ef1108b4b
Mark Canlas <github@htmlism.com>
2013-11-06 00:46:18 -0500

    implemented basic job level check

--- src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
+++ src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
@@ -3,7 +3,14 @@ package com.htmlism.ramza
 case class ZodiacWarrior(jobPoints: Map[JobId, Int] = Map(1 -> 0, 2 -> 0), experiencePoints: Int = 100) {
   def level = experiencePoints / 100
 
-  def jobLevel(job: JobId, minimumsToCheck: List[Int] = jobPointMinimums, level: Int = 1) = ???
+  def jobLevel(job: JobId, minimumsToCheck: List[Int] = jobPointMinimums, level: Int = 1): Int = minimumsToCheck match {
+    case jobPointMinimum :: remainingMinimums =>
+      if (jobPoints(job) >= jobPointMinimum)
+        jobLevel(job, remainingMinimums, level + 1)
+      else
+        level
+    case Nil => level
+  }
 
   def availableJobs = ???
 }
