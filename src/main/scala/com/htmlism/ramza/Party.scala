dcd74d0b5926f0b738f14f8a4d6cacbd3938dd6c
Mark Canlas <github@htmlism.com>
2013-11-09 11:02:13 -0500

    implemented basic map over available jobs

--- src/main/scala/com/htmlism/ramza/Party.scala
+++ src/main/scala/com/htmlism/ramza/Party.scala
@@ -4,12 +4,16 @@ import com.htmlism.ramza.Jobs.JobClass
 import scala.annotation.tailrec
 
 case class Party(characters: ZodiacWarrior*) {
-  private def gainExperienceByJob(parties: Parties, character: ZodiacWarrior, jobs: Traversable[JobClass]) = ???
+  private def gainExperienceByJob(parties: Parties, character: ZodiacWarrior, jobs: Traversable[JobClass]) = parties.flatMap {
+    p => jobs.toList.map {
+      j => p
+    }
+  }
 
   @tailrec
-  private def gainExperienceByCharacter(parties: Parties, characters: Traversable[ZodiacWarrior]): Parties = characters match {
+  private def gainExperienceByCharacter(parties: Parties, characters: Traversable[ZodiacWarrior]): Parties = characters.toList match {
     case head :: tail => {
-      val partiesFromOneCharacter = gainExperienceByJob(parties, head, ???)
+      val partiesFromOneCharacter = gainExperienceByJob(parties, head, head.availableJobs)
 
       gainExperienceByCharacter(partiesFromOneCharacter, tail)
     }
