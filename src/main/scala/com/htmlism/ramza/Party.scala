96ae15c1247815220b4316af919b6d49933cf34a
Mark Canlas <github@htmlism.com>
2013-11-10 19:29:35 -0500

    ordinality is important, mmmkay?

--- src/main/scala/com/htmlism/ramza/Party.scala
+++ src/main/scala/com/htmlism/ramza/Party.scala
@@ -6,21 +6,21 @@ import scala.annotation.tailrec
 case class Party(characters: ZodiacWarrior*) {
   private def gainExperienceByJob = ???
 
-  private def gainExperienceByCharacter(parties: Parties, character: ZodiacWarrior, jobs: Traversable[JobClass]) = parties.flatMap {
+  private def gainExperienceByCharacter(parties: Parties, characterWithIndex: (ZodiacWarrior, Int), jobs: Traversable[JobClass]) = parties.flatMap {
     p => jobs.toList.map {
       j => p
     }
   }
 
   @tailrec
-  private def gainExperienceRecursively(parties: Parties, characters: Traversable[ZodiacWarrior]): Parties = characters.toList match {
+  private def gainExperienceRecursively(parties: Parties, characters: Traversable[(ZodiacWarrior, Int)]): Parties = characters.toList match {
     case head :: tail => {
-      val partiesFromOneCharacter = gainExperienceByCharacter(parties, head, head.availableJobs)
+      val partiesFromOneCharacter = gainExperienceByCharacter(parties, head, head._1.availableJobs)
 
       gainExperienceRecursively(partiesFromOneCharacter, tail)
     }
     case Nil => parties
   }
 
-  def gainExperience: Parties = gainExperienceRecursively(this :: Nil, characters)
+  def gainExperience: Parties = gainExperienceRecursively(this :: Nil, characters.zipWithIndex)
 }
