012c75a9ad1814a7e159b4b5f809a1ded2566ac1
Mark Canlas <github@htmlism.com>
2013-11-11 23:29:45 -0500

    implemented gain experience by job

--- src/main/scala/com/htmlism/ramza/Party.scala
+++ src/main/scala/com/htmlism/ramza/Party.scala
@@ -1,14 +1,36 @@
 package com.htmlism.ramza
 
 import scala.annotation.tailrec
+import com.htmlism.ramza.Jobs.JobClass
 
 case class Party(characters: ZodiacWarrior*) {
-  private def gainExperienceByJob = ???
+  private def gainExperienceByJob(party: Party, character: ZodiacWarrior, index: Int, job: JobClass) = {
+    Party(
+      party.characters.zipWithIndex.map {
+        case (ally, allyIndex) => {
+          val currentJp = ally.jobPoints(job)
+
+          val jpToGain = 8 + character.jobLevel(job) * 2 + character.level / 4
+          val augmentedJpToGain = jpToGain * 3 / 2
+
+          if (allyIndex == index) {
+            ZodiacWarrior(ally.experiencePoints + 10, character.jobs + (job -> (currentJp + augmentedJpToGain)))
+          } else {
+            ally.copy(jobs = ally.jobs + (job -> (currentJp + jpToGain / 4)))
+          }
+        }
+      }: _*
+    )
+  }
 
   private def gainExperienceByCharacter(parties: Parties, characterWithIndex: (ZodiacWarrior, Int)) = {
     val (character, index) = characterWithIndex
 
-    parties.flatMap { p => character.availableJobs.toList.map { j => p } }
+    parties.flatMap { p =>
+      character.availableJobs.toList.map { j =>
+        gainExperienceByJob(p, character, index, j)
+      }
+    }
   }
 
   @tailrec
