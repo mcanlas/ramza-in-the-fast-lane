10a677cd1ce51e7589d6c35f63397cab0cec2445
Mark Canlas <github@htmlism.com>
2013-11-11 23:21:19 -0500

    added job points utility method

--- src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
+++ src/main/scala/com/htmlism/ramza/ZodiacWarrior.scala
@@ -8,15 +8,17 @@ case class ZodiacWarrior(experiencePoints: Int = 100, jobs: Map[JobClass, Int] =
 
   @tailrec
   final def jobLevel(job: JobClass, minimumsToCheck: List[Int] = jobPointMinima, level: Int = 1): Int = minimumsToCheck match {
-    case jobPointMinimum :: remainingMinima if jobs.contains(job) && jobs(job) >= jobPointMinimum => jobLevel(job, remainingMinima, level + 1)
+    case jobPointMinimum :: remainingMinima if jobPoints(job) >= jobPointMinimum => jobLevel(job, remainingMinima, level + 1)
     case _ => level
   }
 
   def availableJobs = prerequisites.keys.filter({ jobName =>
     prerequisites(jobName).forall({
-      case (prerequisiteName, prerequisiteLevel) => jobs.getOrElse(prerequisiteName, 0) >= prerequisiteLevel
+      case (prerequisiteName, prerequisiteLevel) => jobPoints(prerequisiteName) >= prerequisiteLevel
     })
   })
+
+  def jobPoints(job: JobClass) = jobs.getOrElse(job, 0)
 
   def prettyPrint {
     println("Experience: " + experiencePoints)
