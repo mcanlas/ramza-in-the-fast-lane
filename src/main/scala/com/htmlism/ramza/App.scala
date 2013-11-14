8f610e5678e829e1f644fea19824243413ebfdcc
Mark Canlas <github@htmlism.com>
2013-11-14 09:04:12 -0500

    added heuristic based search

--- src/main/scala/com/htmlism/ramza/App.scala
+++ src/main/scala/com/htmlism/ramza/App.scala
@@ -1,5 +1,6 @@
 package com.htmlism.ramza
 
+import com.htmlism.ramza.Jobs.DarkKnight
 import scala.annotation.tailrec
 
 object Main extends App {
@@ -16,9 +17,22 @@ object Main extends App {
   @tailrec
   def gainExperience(i: Int, set: Parties = defaultParty, frontier: Parties = Set.empty): Parties = {
     if (i > 0) {
-     val frontier = set.flatMap(x => x.gainExperience)
+      val frontier = set.flatMap(x => x.gainExperience)
+      println(i)
 
-      gainExperience(i - 1, set ++ frontier, frontier -- set)
+      if (frontier.isEmpty) throw new Exception("hello")
+
+      val frontierByDistance = frontier.groupBy(_.anyDistanceFrom(DarkKnight))
+      val trimmedFrontier = frontierByDistance(frontierByDistance.keys.min)
+
+      if (trimmedFrontier.isEmpty) throw new Exception("zoo!!")
+
+      println("Frontier size is: " + frontier.size)
+      println("Trimmed frontier size is: " + trimmedFrontier.size)
+      println("Search space reduction is: " + (frontier.size - trimmedFrontier.size).asInstanceOf[Double] / frontier.size * 100)
+      println("\n\n\n")
+
+      gainExperience(i - 1, trimmedFrontier, trimmedFrontier)
     } else {
       frontier
     }
