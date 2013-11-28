fe829411f15201f2fe3712f92318535bee8a405c
Mark Canlas <github@htmlism.com>
2013-11-28 11:08:50 -0500

    added gain experience test

--- src/test/scala/com/htmlism/ramza/PartySpec.scala
+++ src/test/scala/com/htmlism/ramza/PartySpec.scala
@@ -1,11 +1,18 @@
 package com.htmlism.ramza
 
 import org.specs2.mutable.Specification
+import com.htmlism.ramza.Jobs._
 
 class PartySpec extends Specification {
   "An empty party" should {
     "yield one solution" in {
       Party().gainExperience.size == 1
+    }
+  }
+
+  "A party of one" should {
+    "incorrectly yield two solutions" in {
+      Party(ZodiacWarrior toSolve Squire).gainExperience.length == 2
     }
   }
 }
