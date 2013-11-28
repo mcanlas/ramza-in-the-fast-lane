39de06434afa4ea4310e208c9d90bbf6ef664b45
Mark Canlas <github@htmlism.com>
2013-11-27 20:17:13 -0500

    added empty party test

--- src/test/scala/com/htmlism/ramza/PartySpec.scala
+++ src/test/scala/com/htmlism/ramza/PartySpec.scala
@@ -3,5 +3,9 @@ package com.htmlism.ramza
 import org.specs2.mutable.Specification
 
 class PartySpec extends Specification {
-
+  "An empty party" should {
+    "yield one solution" in {
+      Party().gainExperience.size == 1
+    }
+  }
 }
